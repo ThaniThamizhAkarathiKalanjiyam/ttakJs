2d03b71df5e01c02922b87f6a119a0a6bade9d49
your username <pitchaimbox-github@yahoo.com>
2020-04-12 18:15:32 +0530

    eve

--- agarathiV2.js
+++ agarathiV2.js
@@ -2,20 +2,9 @@ $(document).ready(function() {
 	
 var md = window.markdownit();
 
-$("#btnSearch").click( function(){
-searchWord();
-});
-
-$("#txtSearch").keypress( function(){
-if (event.which == 13)
-{
-searchWord();
-}
-});
-
 searchWord = function() {
 	$( "#ResultDict" ).html( "Please wait . . . " );
-var txtsearchLow = $("#txtsearch").val().toLowerCase()
+	var txtsearchLow = $("#txtsearch").val().toLowerCase()
 	var tamil_letters = get_tamil_letters(txtsearchLow);
 
 	$.ajaxSetup({
@@ -34,33 +23,42 @@ var txtsearchLow = $("#txtsearch").val().toLowerCase()
 	  }
 	}
 
-// var gitHubUrl = "https://raw.githubusercontent.com/ThaniThamizhAkarathiKalanjiyam/agarathi/master/search/"+tamil_letters[0]+"/"+$("#txtsearch").val().toLowerCase()
-
 var gitHubUrl = "https://ThaniThamizhAkarathiKalanjiyam.github.io/agarathi/search/"+tamil_letters[0]+"/"+ txtsearchLow
-var gitHubUrlOpenDict = "https://github.com/ThaniThamizhAkarathiKalanjiyam/agarathi"
-	$.get(gitHubUrl , 
-  
+//var gitHubUrlOpenDict = "https://github.com/ThaniThamizhAkarathiKalanjiyam/agarathi"
+	
+	$.get(gitHubUrl ,   
 	function( data ) {
 		if(data.length == 0)
 		{
 			$( "#ResultDict" ).html("The Result not available");	  
 		}else{
 			var result = md.render(data);
-			result += "<br/>Thanks <a href="+gitHubUrlOpenDict+">Open Sourced Tamil Dictionary</a>"
+			//result += "<br/>Thanks <a href="+gitHubUrlOpenDict+">Open Sourced Tamil Dictionary</a>"
 			$( "#ResultDict" ).html( result );	  
 		}	  
-	});
-	
+	});	
 };
+
+
 gitHubUrl = "https://thanithamizhakarathikalanjiyam.github.io/ttakJs/tabs.html"
-$.get(gitHubUrl , 
-  
+$.get(gitHubUrl ,   
 	function( data ) {
 		if(data.length == 0)
 		{
 			$( "#ResultDict" ).html( data );	    
 		}	  
 	});
+
+$("#btnSearch").click( function(){
+	searchWord();
+});
+
+$("#txtSearch").keypress( function(){
+	if (event.which == 13)
+	{
+		searchWord();
+	}
+});
 
 $("#txtSearch").focus();
 });
