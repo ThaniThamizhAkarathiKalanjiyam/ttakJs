9faafba978eed1d20469b53d17f81e48d53867a3
mpm2050 <pitchaimbox-travel@yahoo.co.in>
2020-07-24 16:11:14 +0530

    sfsdfsdf

--- iwn.js
+++ iwn.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 	$("#btnSearch").click(function(){
 		$txtSearchVal = $("#txtSearch").val()
 		 $.ajax({
-				url: 'https://thanithamizhakarathikalanjiyam.github.io/iwn/wn_synset/entity',
+				url: 'https://thanithamizhakarathikalanjiyam.github.io/iwn/wn_synset/'+$txtSearchVal,
 				type: 'GET',
 				success: function (ResponseJsonE) {
-					
 					$response = Json.Parse(ResponseJsonE)
+					alert($response)
 				},
 				error: function () {
 					//$dfd.reject();
