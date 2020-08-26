e10d2a49f7bdebed35d421bbfffb9f5438d65d8a
ibizcsdev <pitchaimuthu@ibizcs.com>
2020-08-26 19:19:30 +0530

    date

--- thanithamizhakarathikalanjiyam.js
+++ thanithamizhakarathikalanjiyam.js
@@ -97,7 +97,7 @@ $(document).ready(function () {
 
                     $("#card_title_" + id_card).html(txtsearchLow);
                     $("#card_text_" + id_card).html(replaceIlakText2Links(content));
-                    popup_poem("#card_text_" + id_card)
+                    //popup_poem("#card_text_" + id_card)
                     $("#card_footer_" + id_card).html("");
 
                     if (searchUrl.id == "ResultWNDict") {}
@@ -106,6 +106,81 @@ $(document).ready(function () {
 
     };
 
+ getAllUrlParams = function(url) {
+
+  // get query string from url (optional) or window
+  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+  // we'll store the parameters here
+  var obj = {};
+
+  // if query string exists
+  if (queryString) {
+
+    // stuff after # is not part of query string, so get rid of it
+    queryString = queryString.split('#')[0];
+
+    // split our query string into its component parts
+    var arr = queryString.split('&');
+
+    for (var i = 0; i < arr.length; i++) {
+      // separate the keys and the values
+      var a = arr[i].split('=');
+
+      // set parameter name and value (use 'true' if empty)
+      var paramName = a[0];
+      var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+      // (optional) keep case consistent
+      paramName = paramName.toLowerCase();
+      if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+
+      // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+      if (paramName.match(/\[(\d+)?\]$/)) {
+
+        // create key if it doesn't exist
+        var key = paramName.replace(/\[(\d+)?\]/, '');
+        if (!obj[key]) obj[key] = [];
+
+        // if it's an indexed array e.g. colors[2]
+        if (paramName.match(/\[\d+\]$/)) {
+          // get the index value and add the entry at the appropriate position
+          var index = /\[(\d+)\]/.exec(paramName)[1];
+          obj[key][index] = paramValue;
+        } else {
+          // otherwise add the value to the end of the array
+          obj[key].push(paramValue);
+        }
+      } else {
+        // we're dealing with a string
+        if (!obj[paramName]) {
+          // if it doesn't exist, create property
+          obj[paramName] = paramValue;
+        } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+          // if property does exist and it's a string, convert it to an array
+          obj[paramName] = [obj[paramName]];
+          obj[paramName].push(paramValue);
+        } else {
+          // otherwise add the property
+          obj[paramName].push(paramValue);
+        }
+      }
+    }
+  }
+
+  return obj;
+}
+
+init_text_click_event  = function(){
+
+	var getAllUrlParams_url      = window.location.href; 
+	var searchString  =  getAllUrlParams(getAllUrlParams_url).q
+	if(searchString !== undefined)
+	{
+		$("#txtsearch").val(searchString)
+		$("#btnSearch").trigger("click")
+	}
+}
     init_click_event = function () {
         for (i = 0; i < searchUrls.length; i++) {
             searchWord(searchUrls[i]);
@@ -117,6 +192,8 @@ $(document).ready(function () {
     });
 
     $.when(
-        init_getJSON()).then(function () {});
+        init_getJSON()).then(function () {
+			init_text_click_event();
+		});
 
 });
