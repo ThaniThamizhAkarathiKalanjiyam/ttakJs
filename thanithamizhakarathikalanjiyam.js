1ef3608b487761390752147f22b1ac8378e4a3fc
ibizcsdev <pitchaimuthu@ibizcs.com>
2020-08-26 19:30:03 +0530

    date

--- thanithamizhakarathikalanjiyam.js
+++ thanithamizhakarathikalanjiyam.js
@@ -1,16 +1,13 @@
 $(document).ready(function () {
-
     var bseWeb = "https://ThaniThamizhAkarathiKalanjiyam.github.io/";
     var bseSearchDir = "https://ThaniThamizhAkarathiKalanjiyam.github.io/agarathi/";
     var md = window.markdownit();
     var searchUrls = [];
-
     init_getJSON = function () {
         $.getJSON("https://thanithamizhakarathikalanjiyam.github.io/ttakJs/urls.json", function (data) {
             searchUrls = data;
             var sidenav_left_html = "";
             var nav_tabs_content_html = "";
-
             for (i = 0; i < searchUrls.length; i++) {
                 var id = searchUrls[i].id;
                 var dict = searchUrls[i].dict;
@@ -22,9 +19,7 @@ $(document).ready(function () {
                     active_class = "active"
                         show_active_class = "show active"
                 }
-
                 sidenav_left_html += "<div class='form-check'><input class='form-check-input' type='checkbox' value='' id='sideLink" + id + "'> <label class='form-check-label' for='defaultCheck1'>" + dict + "</label></div>"
-
                 var panColor = "default"
                     var colInt = i % 6
                     if (colInt == 0) {
@@ -40,21 +35,15 @@ $(document).ready(function () {
                     } else {
                         panColor = "default"
                     }
-
                     nav_tabs_content_html += "<div class='card_div_elem panel panel-" + panColor + "' id='panel_" + id + "' style='display:block;'><div class='panel-heading' id='card_header_" + id + "'>Panel Heading</div><div class='panel-body' id='card_text_" + id + "' style='overflow:auto;'></div></div>"
             }
-
             $("#sidenav_left").html(sidenav_left_html)
             //$("#nav_tabs_content_html").html(nav_tabs_content_html)
             $("#nav_tabs_content_html").html(nav_tabs_content_html)
-
             //btnSearch_click(searchUrls)
-
         });
     }
-
     searchWord = function (searchUrl) {
-
         var id = "#" + searchUrl.id
             var id_card = searchUrl.id
             var dict = searchUrl.dict;
@@ -72,7 +61,6 @@ $(document).ready(function () {
             // $(id).html("Please wait . . . ");
             var txtsearchLow = $("#txtsearch").val().toLowerCase()
             var tamil_letters = get_tamil_letters(txtsearchLow);
-
         var gitHubUrl = ""
             if (searchUrl.id == "ResultDict" ||
                 searchUrl.id == "ResultDictTamKal") {
@@ -82,7 +70,6 @@ $(document).ready(function () {
             }
             var pan_id = "#panel_" + id_card
             $(pan_id).css("display", "none")
-
             $.get(gitHubUrl,
                 function (data) {
                 if (data.length == 0) {
@@ -91,109 +78,90 @@ $(document).ready(function () {
                     var result = md.render(data);
                     content = content + result;
                     $("#panel_" + id_card).css("display", "block")
-
                     $("#card_header_" + id_card).html(dict);
                     $("#card_header_" + id_card).append("<span class='material-icons copy_content_str' title='Copy'>content_copy</span>");
-
                     $("#card_title_" + id_card).html(txtsearchLow);
                     $("#card_text_" + id_card).html(content);
                     //popup_poem("#card_text_" + id_card)
                     $("#card_footer_" + id_card).html("");
-
                     if (searchUrl.id == "ResultWNDict") {}
                 }
             });
-
     };
-
- getAllUrlParams = function(url) {
-
-  // get query string from url (optional) or window
-  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-
-  // we'll store the parameters here
-  var obj = {};
-
-  // if query string exists
-  if (queryString) {
-
-    // stuff after # is not part of query string, so get rid of it
-    queryString = queryString.split('#')[0];
-
-    // split our query string into its component parts
-    var arr = queryString.split('&');
-
-    for (var i = 0; i < arr.length; i++) {
-      // separate the keys and the values
-      var a = arr[i].split('=');
-
-      // set parameter name and value (use 'true' if empty)
-      var paramName = a[0];
-      var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-
-      // (optional) keep case consistent
-      paramName = paramName.toLowerCase();
-      if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
-
-      // if the paramName ends with square brackets, e.g. colors[] or colors[2]
-      if (paramName.match(/\[(\d+)?\]$/)) {
-
-        // create key if it doesn't exist
-        var key = paramName.replace(/\[(\d+)?\]/, '');
-        if (!obj[key]) obj[key] = [];
-
-        // if it's an indexed array e.g. colors[2]
-        if (paramName.match(/\[\d+\]$/)) {
-          // get the index value and add the entry at the appropriate position
-          var index = /\[(\d+)\]/.exec(paramName)[1];
-          obj[key][index] = paramValue;
-        } else {
-          // otherwise add the value to the end of the array
-          obj[key].push(paramValue);
+    getAllUrlParams = function (url) {
+        // get query string from url (optional) or window
+        var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+        // we'll store the parameters here
+        var obj = {};
+        // if query string exists
+        if (queryString) {
+            // stuff after # is not part of query string, so get rid of it
+            queryString = queryString.split('#')[0];
+            // split our query string into its component parts
+            var arr = queryString.split('&');
+            for (var i = 0; i < arr.length; i++) {
+                // separate the keys and the values
+                var a = arr[i].split('=');
+                // set parameter name and value (use 'true' if empty)
+                var paramName = a[0];
+                var paramValue = typeof(a[1]) === 'undefined' ? true : a[1];
+                // (optional) keep case consistent
+                paramName = paramName.toLowerCase();
+                if (typeof paramValue === 'string')
+                    paramValue = paramValue.toLowerCase();
+                // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+                if (paramName.match(/\[(\d+)?\]$/)) {
+                    // create key if it doesn't exist
+                    var key = paramName.replace(/\[(\d+)?\]/, '');
+                    if (!obj[key])
+                        obj[key] = [];
+                    // if it's an indexed array e.g. colors[2]
+                    if (paramName.match(/\[\d+\]$/)) {
+                        // get the index value and add the entry at the appropriate position
+                        var index = /\[(\d+)\]/.exec(paramName)[1];
+                        obj[key][index] = paramValue;
+                    } else {
+                        // otherwise add the value to the end of the array
+                        obj[key].push(paramValue);
+                    }
+                } else {
+                    // we're dealing with a string
+                    if (!obj[paramName]) {
+                        // if it doesn't exist, create property
+                        obj[paramName] = paramValue;
+                    } else if (obj[paramName] && typeof obj[paramName] === 'string') {
+                        // if property does exist and it's a string, convert it to an array
+                        obj[paramName] = [obj[paramName]];
+                        obj[paramName].push(paramValue);
+                    } else {
+                        // otherwise add the property
+                        obj[paramName].push(paramValue);
+                    }
+                }
+            }
         }
-      } else {
-        // we're dealing with a string
-        if (!obj[paramName]) {
-          // if it doesn't exist, create property
-          obj[paramName] = paramValue;
-        } else if (obj[paramName] && typeof obj[paramName] === 'string'){
-          // if property does exist and it's a string, convert it to an array
-          obj[paramName] = [obj[paramName]];
-          obj[paramName].push(paramValue);
-        } else {
-          // otherwise add the property
-          obj[paramName].push(paramValue);
-        }
-      }
+        return obj;
     }
-  }
-
-  return obj;
-}
-
-init_text_click_event  = function(){
-
-	var getAllUrlParams_url      = window.location.href; 
-	var searchString  =  getAllUrlParams(getAllUrlParams_url).q
-	if(searchString !== undefined)
-	{
-		$("#txtsearch").val(searchString)
-		$("#btnSearch").trigger("click")
-	}
-}
+    init_text_click_event = function () {
+        var getAllUrlParams_url = window.location.href;
+        var searchString = getAllUrlParams(getAllUrlParams_url).q
+            if (searchString !== undefined) {
+                $("#txtsearch").val(searchString)
+                $("#btnSearch").trigger("click")
+            }
+    }
     init_click_event = function () {
         for (i = 0; i < searchUrls.length; i++) {
             searchWord(searchUrls[i]);
         }
     };
-
     $("#btnSearch").click(function () {
         init_click_event()
     });
-
-    $.when(
-        init_getJSON()).then(function () {
-			init_text_click_event();
-		});
-
+    var jqxhr = $.when(
+            init_getJSON()).then(function () {});
+    // Set another completion function for the request above
+    jqxhr.always(function () {
+        init_text_click_event();
+    });
 });
