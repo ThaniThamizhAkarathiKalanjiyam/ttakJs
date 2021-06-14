db2dfa7b4d3425dac0f99e32ff3e0012c4211d98
mpm2050 <pitchaimbox-travel@yahoo.co.in>
2021-06-14 19:09:50 +0530

    Update thanithamizhakarathikalanjiyam.js

--- thanithamizhakarathikalanjiyam.js
+++ thanithamizhakarathikalanjiyam.js
@@ -395,7 +395,7 @@ $(document).ready(function () {
         .done(function () {
             //versol_div("வேர்", ["இடது கிளை", "வலது கிளை"]);
 			$('#jstree_demo_div').jstree();
-            side_extra_info();			
+            // side_extra_info();			
         });
     // Set another completion function for the request above
     jqxhr.always(function () {});
