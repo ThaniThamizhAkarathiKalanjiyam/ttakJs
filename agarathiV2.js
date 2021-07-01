7c48bbbbf0bb30c43dd6ae0b43f24fa65841f835
mpm2050 <pitchaimbox-travel@yahoo.co.in>
2021-07-01 09:40:37 +0530

    Update agarathiV2.js

--- agarathiV2.js
+++ agarathiV2.js
@@ -1,5 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    GetAgarathiV2 = function (funcData) {
+        $.ajax({
+            url: 'Process',
+            type: 'POST',
+            dataType: 'json',
+            data: {},
+            success: function (ResponseJsonE) {},
+            error: function () {}
+        });
+    }
 
+    //Initial method call
+    var jqxhr = $.when().then(function () {
+            GetAgarathiV2()
+        });
+    // Set another completion function for the request above
+    jqxhr.always(function () {
+        //Elements event Functionality Starts
+        //Elements event Functionality Ends
+
+    });
 
 });
