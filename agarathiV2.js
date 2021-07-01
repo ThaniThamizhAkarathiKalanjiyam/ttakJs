fba23fe5d2bfa45d06b1fc7698de1a1f89a014f2
mpm2050 <pitchaimbox-travel@yahoo.co.in>
2021-07-01 10:14:41 +0530

    Update agarathiV2.js

--- agarathiV2.js
+++ agarathiV2.js
@@ -1,19 +1,26 @@
 $(document).ready(function () {
 
     GetAgarathiV2 = function (funcData) {
-		
-		$.getJSON("https://thanithamizhakarathikalanjiyam.github.io/ttakJs/tam_letters.json", function (ResponseJsonE) {
-			
-			$.each(ResponseJsonE.tam_first_letters,function(index, value){
-										
-					$('#sel_first_letter').append($('<option>',
-					 {
-						value: value,
-						text : value
-					}));
-				});
-			
-		});
+
+        $.getJSON("https://thanithamizhakarathikalanjiyam.github.io/ttakJs/tam_letters.json", function (ResponseJsonE) {
+
+            $.each(ResponseJsonE.tam_first_letters, function (index, value) {
+
+                $('#sel_first_letter').append($('<option>', {
+                        value: value,
+                        text: value
+                    }));
+            });
+
+            $.each(ResponseJsonE.tam_last_letters, function (index, value) {
+
+                $('#sel_last_letter').append($('<option>', {
+                        value: value,
+                        text: value
+                    }));
+            });
+
+        });
     }
 
     //Initial method call
