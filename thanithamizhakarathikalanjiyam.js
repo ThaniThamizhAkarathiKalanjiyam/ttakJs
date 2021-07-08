0654a0df3d194734c035639e132605793c01b951
mpm2050 <pitchaimbox-travel@yahoo.co.in>
2021-07-08 11:22:42 +0530

    ggfhgg

--- thanithamizhakarathikalanjiyam.js
+++ thanithamizhakarathikalanjiyam.js
@@ -405,9 +405,45 @@ $(document).ready(function () {
     // Set another completion function for the request above
     jqxhr.always(function () {
 		
-	
+	google.charts.load('current', {packages:['wordtree']});
+      google.charts.setOnLoadCallback(drawChart);
 		
-	});
-	
+	});      
+
+   drawChart =   function () {
+        var data = google.visualization.arrayToDataTable(
+          [ ['Phrases'],
+            ['cats are better than dogs'],
+            ['cats eat kibble'],
+            ['cats are better than hamsters'],
+            ['cats are awesome'],
+            ['cats are people too'],
+            ['cats eat mice'],
+            ['cats meowing'],
+            ['cats in the cradle'],
+            ['cats eat mice'],
+            ['cats in the cradle lyrics'],
+            ['cats eat kibble'],
+            ['cats for adoption'],
+            ['cats are family'],
+            ['cats eat mice'],
+            ['cats are better than kittens'],
+            ['cats are evil'],
+            ['cats are weird'],
+            ['cats eat mice'],
+          ]
+        );
+
+        var options = {
+          wordtree: {
+            format: 'implicit',
+            word: 'cats'
+          }
+        };
+
+        var chart = new google.visualization.WordTree(document.getElementById('wordtree_basic'));
+        chart.draw(data, options);
+      }
+   
 	
 });
